877916e041f4dfcadaf302dc624d79de5a8f2935
Srinandhini M <srinandhini@sedintechnologies.com>
2022-09-22 15:36:11 +0530

    Renamed the Variables

--- combination_sum.rb
+++ combination_sum.rb
@@ -4,19 +4,19 @@ def combination_sum(candidates, target)
     end
     backtracking(candidates, target)
 end
-def backtracking(candidates, target, index = 0, path = [], result = [], total = 0)
-    if total == target
+def backtracking(candidates, target, index = 0, path = [], result = [], sum = 0)
+    if sum == target
         result.push(path)
         return
     end
     index.upto(candidates.size - 1) do |i|
           next if total + candidates[i] >  target
-        backtracking(candidates, target, i, path + [candidates[i]], result, total + candidates[i])
+        backtracking(candidates, target, i, path + [candidates[i]], result, sum + candidates[i])
     end
     result
 end
 
-p combination_sum([2,3,6,7],7)
+p combination_sum([1,2,3,6,7],7)
 
 
 
